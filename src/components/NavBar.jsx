10b4b4a7371e6e586f19ab6b0059c2d38d8451a4
samito <martinsamuelpp@gmail.com>
2024-05-14 09:49:46 -0600

    correccion en altura del NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,7 +23,7 @@ const NavBar = () => {
         <Fade damping={.5} delay={100}>
             <header className="flex justify-between w-full absolute lg:justify-center">
                 <nav className={`${menu ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0 px-9 transition-all duration-300 ease-in-out w-1/2 h-screen lg:h-20 pb-5 relative ssm:bg-customWhiteLight lg:bg-transparent ssm:shadow-lg lg:shadow-none`}>
-                    <ul className="sm:flex-col lg:flex lg:flex-row lg:gap-8 lg:translate-y-4 w-full h-screen justify-center">
+                    <ul className="sm:flex-col lg:flex lg:flex-row lg:gap-8 lg:translate-y-4 w-full ssm:h-screen lg:h-10 justify-center">
                         <li className="pt-5"><a className="hover:text-customBlackLight transition duration-200 text-customBlack" href="#banner" onClick={() => { handleMenuItemClick("banner") }}>inicio</a></li>
                         <li className="pt-5"><a className="hover:text-customBlackLight transition duration-200 text-customBlack" href="#mimundo" onClick={() => { handleMenuItemClick("mimundo") }}>mi mundo</a></li>
                         <li className="pt-5"><a className="hover:text-customBlackLight transition duration-200 text-customBlack" href="#proyects" onClick={() => { handleMenuItemClick("") }}>proyectos</a></li>
