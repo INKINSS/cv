7cdfe07e90ae3b8d86f82992cd3e0501da27e591
samito <martinsamuelpp@gmail.com>
2024-05-13 21:39:47 -0600

    apartado de proyectos hecho con detalles que mejorear en data

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Banner from './components/Banner'
 import MiMundo from './components/MiMundo'
 import NavBar from './components/NavBar'
+import Proyects from './components/Proyects'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <body className='absolute translate-y-16 -z-10 ssm:px-14 lg:translate-y-40 md:px-40'>
         <Banner />
         <MiMundo />
+        <Proyects />
       </body>
     </main>
   )
---
+++ src/components/Proyects.jsx
@@ -0,0 +1,10 @@
+
+const Proyects = () => {
+    return (
+        <article className="mt-16">
+            <h3 className="text-customBlack ssm:text-xl lg:text-2xl font-semibold mb-4">proyectos</h3>
+        </article>
+    )
+}
+
+export default Proyects
